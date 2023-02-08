54a1d69cc2e1a9e22faa813f8dcca30e243fc956
Anastasia Samoilova <86anastasias@gmail.com>
2023-02-07 17:12:11 -0700

    initial

---
+++ src/db_service/actions/io.py
@@ -0,0 +1,19 @@
+"""
+Request-Response schema
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Request:
+    """
+    Abstract class for an action request
+    """
+
+
+@dataclass
+class Response:
+    """
+    Abstract class for an action response
+    """
